c589091d1a3e7392636688cabc1dbfe46f93dd3f
sorrowbeaver <jkj8790@gmail.com>
2016-09-24 20:58:32 +0900

    max값만 표시하도록

--- test_script.sh
+++ test_script.sh
@@ -6,11 +6,9 @@ THREE_FILE=./test_result_three.txt
 SCRIPT_FILE=scripts2
 rm grep.gcda
 
-results=()
-result_three=()
-scripts=()
 
 # make test case file into string array
+scripts=()
 while read line
 do
 	if [ -n "$line" ] && [ "${line:0:1}" != "#" ]
@@ -20,10 +18,9 @@ do
 done < $SCRIPT_FILE
 
 # execute each test case
+case_max=0
 for value in "${scripts[@]}"; do
-	echo "$value"
-	$value
-	echo -e "\n----------------------------------------------------------------------------"
+	$value > /dev/null 2>&1
 	RESULT=`gcov -nb grep.c`
 
 	line=${RESULT#*:}
@@ -31,12 +28,17 @@ for value in "${scripts[@]}"; do
 	branch=${line#*:}
 	branch_percent=${branch%%\%*}
 
-	results+="$value\t\t$branch_percent\n" 
+	if [ $(echo $case_max'<'$branch_percent | bc -l) -eq 1 ]
+	then
+		case_max_script=$value
+		case_max=$branch_percent
+	fi
+	
 	rm grep.gcda
 done
 
 # make test suite which size is 3
-echo ${#scripts[@]}
+suite_max=0
 SCRIPT_SIZE=${#scripts[@]}
 for i in `seq 1 $(($SCRIPT_SIZE - 3))`
 do
@@ -44,9 +46,9 @@ do
 	do
 		for k in `seq $(($j + 1)) $(($SCRIPT_SIZE - 1))`
 		do
-			${scripts[i]}
-			${scripts[j]}
-			${scripts[k]}
+			${scripts[i]} > /dev/null 2>&1
+			${scripts[j]} > /dev/null 2>&1
+			${scripts[k]} > /dev/null 2>&1
 			
 			RESULT=`gcov -nb grep.c`
 
@@ -55,15 +57,15 @@ do
 			branch=${line#*:}
 			branch_percent=${branch%%\%*}
 
-			result_three+="$i,$j,$k \t\t$branch_percent\n" 
+			if [ $(echo $suite_max'<'$branch_percent | bc -l) -eq 1 ]
+			then
+				suite_max_scripts="${scripts[i]}\n${scripts[j]}\n${scripts[k]}"
+				suite_max=$branch_percent
+			fi
+
 			rm grep.gcda
 		done
 	done
-done
-
-rm $FILE
-for value in "${results[@]}"; do
-	echo -e "$value" >> $FILE
 done
 
 rm $THREE_FILE
@@ -71,12 +73,14 @@ for value in "${result_three[@]}"; do
 	echo -e "$value" >> $THREE_FILE
 done
 
-sort -t$'\t' -k3 -g -r $FILE -o $FILE
 
-echo -e "\n\nBranch coverage for each test cases"
+echo -e "\n\nTest case with max branch coverage"
 echo "----------------------------------------------------------------------------"
-echo -e "Test case\t\t\tBranch coverage"
-cat $FILE
+echo -e "$case_max_script\t$case_max"
+
+echo -e "\n\nTest suite with max branch coverage"
+echo "----------------------------------------------------------------------------"
+echo -e "$suite_max_scripts\t$suite_max"
 
 echo -e "\nCoverages for all test cases"
 echo "----------------------------------------------------------------------------"
